5858c4a19bf3e241a60fe4aaf2dca28aec435bd1
SarahJobyDavid <154341671+SarahJobyDavid@users.noreply.github.com>
2025-09-10 00:53:00 +0530

    Time: 1 ms (99.89%), Space: 46.2 MB (8.21%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int finalPos = nums.length-1;
+        for(int i=nums.length-1; i>=0; i--){
+            if(nums[i] + i >= finalPos){
+                finalPos = i;
+            }
+        }  
+        return finalPos == 0;      
+    }
+}
